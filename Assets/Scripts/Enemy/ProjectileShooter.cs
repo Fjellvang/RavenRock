5cc8f118e5b33e60556d0192349e6519000597b5
aleksander fjellvang <asf@appstract.dk>
2021-02-21 16:43:24 +0100

    added turrent enemty

---
+++ Assets/Scripts/Enemy/ProjectileShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileShooter : MonoBehaviour
+{
+    public GameObject Projectile;
+    public float spawnSeconds;
+	public float projectTileSpeed;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        InvokeRepeating(nameof(SpawnProjectile), 0, spawnSeconds);
+    }
+
+    public void SpawnProjectile()
+	{
+        var spawned = Instantiate(Projectile, gameObject.transform.position, gameObject.transform.rotation, gameObject.transform);
+        var spawnedRig = spawned.GetComponent<Rigidbody2D>();
+        spawnedRig.AddForce(gameObject.transform.right * projectTileSpeed);
+	}
+}
