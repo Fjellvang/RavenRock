e6915371cd73db7c12879ae53ba1858d5a57a4d5
aleksander fjellvang <asf@appstract.dk>
2021-02-27 20:10:36 +0100

    prepared farmer for stunned

--- Assets/Scripts/States/EnemyStates/EnemyDeadState.cs
+++ Assets/Scripts/States/EnemyStates/EnemyDeadState.cs
@@ -9,14 +9,11 @@ namespace Assets.Scripts.States.EnemyStates
 {
 	public class EnemyDeadState : EnemyBaseState
 	{
-		AnimatorStateInfo state;
 		public override void OnEnterState(AI controller)
 		{
 			controller.GetComponent<CapsuleCollider2D>().enabled = false;
 			controller.GetComponent<Rigidbody2D>().simulated = false;
 			controller.Animator.SetTrigger("Death");
-			//controller.Animator.PlayInFixedTime("Death");
-			state = controller.Animator.GetCurrentAnimatorStateInfo(0);
 		}
 
 	}
