815698b6bbb8f434468aedf7b7621068e20bae2c
aleksander fjellvang <asf@appstract.dk>
2021-03-07 12:47:44 +0100

    buggy floating text

---
+++ Assets/Scripts/CombatSystem/FloatingDamageText.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.CombatSystem
+{
+	public class FloatingDamageText : MonoBehaviour
+	{
+		public TextMesh textMesh;
+
+		public float duration = 2f;
+		public float speed = 1f;
+
+
+		private void Update()
+		{
+			transform.Translate(Vector3.up * speed * Time.deltaTime); //Todo: Add some horizontal movement ? Maybe with perlinNoise
+
+			duration -= Time.deltaTime;
+			if (duration <= 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
