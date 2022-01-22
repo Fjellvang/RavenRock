9f1cc61bc9c4a40d1811c614689bfdb8390a57b5
aleksander fjellvang <asf@appstract.dk>
2022-01-22 14:37:21 +0100

    Simple Pause implemented

---
+++ Assets/Scripts/Menu/PauseControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
