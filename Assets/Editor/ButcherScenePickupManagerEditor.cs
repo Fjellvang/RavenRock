458b3abcf2fe6e0c142778641b16f8529c689659
Aleksander <alf@umrabco.dk>
2022-05-17 13:37:25 +0200

    assign quad boundary via transform

--- Assets/Editor/ButcherScenePickupManagerEditor.cs
+++ Assets/Editor/ButcherScenePickupManagerEditor.cs
@@ -1,15 +1,28 @@
 ﻿using Assets.Scripts.Enemy.ButcherBoss;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 [CustomEditor(typeof(ButcherScenePickupManager))]
 public class ButcherScenePickupManagerEditor : Editor
 {
+    Transform topLeft = null;
+    Transform bottomRight = null;
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
+
+        var manager = (ButcherScenePickupManager)target;
+
+        topLeft = EditorGUILayout.ObjectField("Top Left", topLeft, typeof(Transform), true) as Transform;
+        bottomRight = EditorGUILayout.ObjectField("Bottom Right", bottomRight, typeof(Transform), true) as Transform;
+
+        if (topLeft != null)
+        {
+            manager.boundary.TopLeft = topLeft.position;
+        }
+        if (bottomRight != null)
+        {
+            manager.boundary.BottomRight = bottomRight.position;
+        }
     }
 }
